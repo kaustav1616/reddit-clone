49c6dece292646cbf3ccb7dac8af7c3b63d83203
kaustav1616 <kaustav1616@yahoo.com>
2021-05-23 13:29:26 +0530

    adding gitignore

--- reddit-backend/src/main/java/com/kaustav/redditbackend/RedditBackendApplication.java
+++ reddit-backend/src/main/java/com/kaustav/redditbackend/RedditBackendApplication.java
@@ -1,10 +1,13 @@
 package com.kaustav.redditbackend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedditBackendApplication {
+@EnableEncryptableProperties
+public class RedditBackendApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedditBackendApplication.class, args);
